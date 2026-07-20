b93ec550590176e220564e768656cab6013766c9
rizalfr <zacksupreme@gmail.com>
2026-07-20 12:02:18 +0700

    Swap Korba mouse thumb buttons

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,12 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// On Korba, keep the stock VI mouse layer but put left click before right
+// click on the right thumb cluster.
+#if defined(MIRYOKU_KEYBOARD_KORBA)
+#define MIRYOKU_LAYER_MOUSE \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_MS_L,            U_MS_D,            U_MS_U,            U_MS_R,            U_NU,              \
+U_NA,              &kp RALT,          &u_to_U_SYM,       &u_to_U_MOUSE,     U_NA,              U_WH_L,            U_WH_D,            U_WH_U,            U_WH_R,            U_NU,              \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_BTN1,            U_BTN2,            U_BTN3,            U_NP,              U_NP
+#endif
